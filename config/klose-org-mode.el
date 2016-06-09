6786ffe69da853ef74a85af4a67e6b993fbcb8c0
klose911 <klose911@gmail.com>
2016-06-10 01:16:03 +0800

    add key-binding for insert src block in org mode

--- config/klose-org-mode.el
+++ config/klose-org-mode.el
@@ -1,38 +1,45 @@
 (require 'org-install)
-;;(require 'org-publish)
 
-;; The following lines are always needed. Choose your own keys.
-(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(defun org-insert-src-block (src-code-type)
+  "Insert a `SRC-CODE-TYPE' type source code block in org-mode."
+  (interactive
+   (let ((src-code-types
+          '("emacs-lisp" "python" "C" "sh" "java" "js" "clojure" "C++" "css"
+            "calc" "asymptote" "dot" "gnuplot" "ledger" "lilypond" "mscgen"
+            "octave" "oz" "plantuml" "R" "sass" "screen" "sql" "awk" "ditaa"
+            "haskell" "latex" "lisp" "matlab" "ocaml" "org" "perl" "ruby"
+            "scheme" "sqlite")))
+     (list (ido-completing-read "Source code type: " src-code-types))))
+  (progn
+    (newline-and-indent)
+    (insert (format "#+BEGIN_SRC %s\n" src-code-type))
+    (newline-and-indent)
+    (insert "#+END_SRC\n")
+    (previous-line 1)
+    (org-edit-src-code)))
+
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode)) 
 (add-hook 'org-mode-hook 'turn-on-font-lock) ; not needed when global-font-lock-mode is on
 (add-hook 'org-mode-hook 
-	  (lambda () (setq truncate-lines nil)))
+	  (lambda ()
+	    (setq truncate-lines nil)
+	    ;; turn on flyspell-mode by default
+	    (flyspell-mode 1) 
+	    ;; C-TAB for expanding
+	    (local-set-key (kbd "C-<tab>")
+			   'yas/expand-from-trigger-key)
+	    ;; keybinding for editing source code blocks
+	    (local-set-key (kbd "C-c s e")
+			   'org-edit-src-code)
+	    ;; keybinding for inserting code blocks
+	    (local-set-key (kbd "C-c s i")
+			   'org-insert-src-block)
+	    (local-set-key (kbd "C-c s l")
+			   'org-store-link)
+	    (local-set-key (kbd "C-c s a")
+			   'org-agenda)
+	    (local-set-key (kbd "C-c s b")
+			   'org-iswitchb)
+	    ))
 
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cb" 'org-iswitchb)
-
-;; (setq org-publish-project-alist
-;;       '(("note-org"
-;;          :base-directory "D:/Dropbox/Document/note/org"
-;;          :publishing-directory "D:/Dropbox/Document/note/org/publish"
-;;          :base-extension "org"
-;;          :recursive t
-;;          :publishing-function org-publish-org-to-html
-;;          :auto-index nil
-;;          :index-filename "index.org"
-;;          :index-title "index"
-;;          :link-home "index.html"
-;;          :section-numbers nil
-;;          :style "<link rel=\"stylesheet\"
-;;     href=\"./style/emacs.css\"
-;;     type=\"text/css\"/>")
-;;         ("note-static"
-;;          :base-directory "D:/Dropbox/Document/note/org"
-;;          :publishing-directory "D:/Dropbox/Document/note/org/publish"
-;;          :recursive t
-;;          :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|swf\\|zip\\|gz\\|txt\\|el"
-;;          :publishing-function org-publish-attachment)
-;;         ("note" 
-;;          :components ("note-org" "note-static")
-;;          :author "klose911@gmail.com"))) 
 
