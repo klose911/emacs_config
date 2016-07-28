0add86abaf706040f7dfeb284ea7d1cbd29f48c4
klose911 <klose911@gmail.com>
2016-07-28 14:07:08 +0800

    add ruby support in windows

--- config/klose-clisp.el
+++ config/klose-clisp.el
@@ -1,21 +1,21 @@
 (if *win* 
-  (progn
-  	(setq inferior-lisp-program "D:/util/clisp/clisp.exe")
-	(setq temporary-file-directory "C:/Documents and Settings/klose.wu/Local Settings/Temp"))
+    (progn
+      (setq inferior-lisp-program "D:/util/clisp/clisp.exe")
+      (setq temporary-file-directory "c:/Documents and Settings/klose.wu/Local Settings/Temp/"))
   (setq inferior-lisp-program "clisp")) 
 
 (add-to-list 'load-path "~/.emacs.d/elisp/slime")
 (require 'slime)
 (slime-setup)
-;(slime) ;M-x slime
+					;(slime) ;M-x slime
 (require 'slime-autoloads)
 (slime-setup '(slime-fancy))
 
 (defun lisp-indent-or-complete (&optional arg)
   (interactive "p")
   (if (or (looking-back "^\\s-*") (bolp))
-	(call-interactively 'lisp-indent-line)
-	(call-interactively 'slime-indent-and-complete-symbol)))
+      (call-interactively 'lisp-indent-line)
+    (call-interactively 'slime-indent-and-complete-symbol)))
 (eval-after-load "lisp-mode"
-				 '(progn
-					(define-key lisp-mode-map (kbd "TAB") 'lisp-indent-or-complete)))
+  '(progn
+     (define-key lisp-mode-map (kbd "TAB") 'lisp-indent-or-complete)))
--- config/klose-ruby.el
+++ config/klose-ruby.el
@@ -4,3 +4,6 @@
 	     '("\\.\\(?:cap\\|gemspec\\|irbrc\\|gemrc\\|rake\\|rb\\|ru\\|thor\\)\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist
 	     '("\\(?:Brewfile\\|Capfile\\|Gemfile\\(?:\\.[a-zA-Z0-9._-]+\\)?\\|[rR]akefile\\)\\'" . ruby-mode))
+
+(if *win* 
+(setq ruby-program-name "D:/Program Files/Ruby193/bin/irb  --inf-ruby-mode"))
