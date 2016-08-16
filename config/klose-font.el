1b21863da2bdbc653c9d035490ee1e53116a750c
klose911 <klose911@gmail.com>
2016-08-16 22:43:07 +0800

    change font setting under linux

--- config/klose-font.el
+++ config/klose-font.el
@@ -1,17 +1,17 @@
-;首先指定默认的英文等宽字体 
-;(set-default-font "DejaVu Sans Mono") 
-(if *mac* 
-  (progn 
-	(set-frame-font "Monaco:pixelsize=18")
-	(dolist (charset '(han kana symbol cjk-misc bopomofo))
-	  (set-fontset-font (frame-parameter nil 'font)
-						charset
-						(font-spec :family "Hiragino Sans GB" :size 20)
-						))) 
-  (custom-set-faces
-	;; custom-set-faces was added by Custom.
-	;; If you edit it by hand, you could mess it up, so be careful.
-	;; Your init file should contain only one such instance.
-	;; If there is more than one, they won't work right.
-	'(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 158 :width normal :foundry "outline" :family "Lucida Sans"))))))
+					;首先指定默认的英文等宽字体 
+					;(set-default-font "DejaVu Sans Mono") 
+(cond (*mac* 
+       (progn 
+	 (set-frame-font "Monaco:pixelsize=18")
+	 (dolist (charset '(han kana symbol cjk-misc bopomofo))
+	   (set-fontset-font (frame-parameter nil 'font)
+			     charset
+			     (font-spec :family "Hiragino Sans GB" :size 20)
+			     ))))
+      (*linux* (custom-set-faces
+		'(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 159 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))))
+
+      (else 	     
+       (custom-set-faces
+	'(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 158 :width normal :foundry "outline" :family "Lucida Sans")))))))
 
