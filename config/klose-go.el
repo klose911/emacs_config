1a45fbf65a6d54d46a38e36a2922e77cf1800252
klose911 <klose911@gmail.com>
2016-12-09 00:49:41 +0800

    go env settings under mac

--- config/klose-go.el
+++ config/klose-go.el
@@ -1,15 +1,19 @@
 (require 'go-mode)
 (with-eval-after-load 'go-mode
-  (require 'go-autocomplete))
+					  (require 'go-autocomplete))
 
 (cond (*win*
-       (progn
-	 (setq exec-path (append exec-path '("d:/util/go/bin")))))
-      (*linux*
-       (progn
-	 (setenv "GOPATH" "/home/klose/Applications/goprojects/")
-	 (setq exec-path (append exec-path '("/home/klose/Applications/goprojects/bin")))))
-      (t nil))
+		(progn
+		  (setq exec-path (append exec-path '("d:/util/go/bin")))))
+	  (*linux*
+		(progn
+		  (setenv "GOPATH" "/home/klose/Applications/goprojects/")
+		  (setq $exec-path (append exec-path '("/home/klose/Applications/goprojects/bin")))))
+	  (*mac*
+		(progn
+		  (setenv "GOPATH" "/Users/klose/Bin/goprojects/")
+		  (setq exec-path (append exec-path '("/Users/klose/Bin/goprojects/bin")))))
+	  (t nil))
 
 (defun my-go-mode-hook ()
   ;; Use goimports instead of go-fmt
@@ -18,8 +22,8 @@
   (add-hook 'before-save-hook 'gofmt-before-save)
   ;; Compile and test go code
   (if (not (string-match "go" compile-command))
-      (set (make-local-variable 'compile-command)
-           "go build -v && go test -v && go vet"))
+	(set (make-local-variable 'compile-command)
+		 "go build -v && go test -v && go vet"))
   ;; Godef jump key binding                                                      
   (local-set-key (kbd "M-.") 'godef-jump)
   (local-set-key (kbd "M-*") 'pop-tag-mark))
