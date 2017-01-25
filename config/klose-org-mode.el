07474a8efac1425c230929f2bda472efa0995f65
klose.wu <klose.wu@ebaotech.com>
2017-01-25 15:43:41 +0800

    define postamble for exported html

--- config/klose-org-mode.el
+++ config/klose-org-mode.el
@@ -20,6 +20,20 @@
     (previous-line 1)
     (org-edit-src-code)))
 
+(defvar klose-html-postamble
+"<p class=\"postamble\">Last Updated %C. Created by %a</p>
+<div id=\"uyan_frame\"></div>
+<script type=\"text/javascript\" src=\"http://v2.uyan.cc/code/uyan.js?uid=2124392\"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-90850421-1', 'auto');
+  ga('send', 'pageview');
+</script>
+")
+
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 (add-to-list 'auto-coding-alist '("\\.org\\'" . utf-8))
 (add-hook 'org-mode-hook 'turn-on-font-lock) ; not needed when global-font-lock-mode is on
