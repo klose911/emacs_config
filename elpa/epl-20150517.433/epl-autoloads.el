990f2ca9993ef697feaca97a127d36c075e235fe
klose911 <klose911@gmail.com>
2017-03-25 20:04:52 +0800

    upgrade installed elpa packages

---
+++ elpa/epl-20150517.433/epl-autoloads.el
@@ -0,0 +1,15 @@
+;;; epl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("epl.el") (22742 23476 474430 712000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; epl-autoloads.el ends here
---
+++ elpa/go-mode-20170308.1512/go-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "go-mode" "20170308.1512" "Major mode for the Go programming language" 'nil :url "https://github.com/dominikh/go-mode.el" :keywords '("languages" "go"))
