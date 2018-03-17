786f2d52641089199841eae6c6c4329f0dc6767c
klose911 <klose911@gmail.com>
2018-03-17 19:28:34 +0800

    disable ecb tips

--- config/klose-ecb.el
+++ config/klose-ecb.el
@@ -1,10 +1,10 @@
 ;;(add-to-list 'load-path "~/.emacs.d/elisp/ecb")
 (require 'ecb)
-;; (custom-set-variables
- ;; custom-set-variables was added by Custom.
+(custom-set-variables
+ ;; ustom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  ;; '(ecb-options-version "2.40")
- ;; '(ecb-tip-of-the-day nil))
+ '(ecb-tip-of-the-day nil))
 
