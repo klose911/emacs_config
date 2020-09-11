218c179c59c70c3a6b334fbfd49f5c84c7402e3e
i514692 <klose.wu@sap.com>
2020-09-11 10:57:58 +0200

    remove all elpa packages

--- elpa/mew-20180710.117/mew-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "mew" "20180710.117" "Messaging in the Emacs World" 'nil :authors
-  '(("Kazu Yamamoto" . "Kazu@Mew.org"))
-  :maintainer
-  '("Kazu Yamamoto" . "Kazu@Mew.org"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
