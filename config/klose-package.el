bc7f4b166aa331549cdae172c46c859187f2d8d6
I514692 <klose.wu@sap.com>
2024-09-24 17:27:58 +0800

    clean the dulicated define of package-selected-packages varible

--- config/klose-package.el
+++ config/klose-package.el
@@ -7,11 +7,6 @@
 
 (package-initialize)
 
-(custom-set-variables
- '(package-selected-packages
-   (quote
-    (kotlin-mode erlang xcscope rust-mode cargo toml-mode py-autopep8 flycheck jedi elpy yaml-mode window-numbering w3m virtualenvwrapper virtualenv slime session racket-mode psgml mpg123 mew markdown-preview-mode magit inf-ruby htmlize go-dlv go-autocomplete folding emms cygwin-mount cider auctex))))
-
 ;; fetch the list of packages available 
 (unless package-archive-contents
   (package-refresh-contents))
